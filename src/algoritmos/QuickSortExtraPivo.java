8a36194b9d12e9faa99b810c94177877c938f35a
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-04 20:27:43 -0300

    fix: add comment

--- src/algoritmos/QuickSortExtraPivo.java
+++ src/algoritmos/QuickSortExtraPivo.java
@@ -17,45 +17,7 @@ public class QuickSortExtraPivo<T extends Comparable<T>> extends AbstractSorting
 
 	@Override
 	public T[] ordena(T[] array, int inicio, int fim) {
-		/*
-		if (right > left) {
-        // Choose outermost elements as pivots
-        if (A[left] > A[right]) swap(A, left, right);
-        int p = A[left], q = A[right];
-
-        // Partition A according to invariant below
-        int l = left + 1, g = right - 1, k = l;
-        while (k <= g) {
-            if (A[k] < p) {
-                swap(A, k, l);
-                ++l;
-            } else if (A[k] >= q) {
-                while (A[g] > q && k < g) --g;
-                swap(A, k, g);
-                --g;
-                if (A[k] < p) {
-                    swap(A, k, l);
-                    ++l;
-                }
-            }
-            ++k;
-        }
-        --l; ++g;
-
-        // Swap pivots to final place
-        swap(A, left, l); swap(A, right, g);
-
-        // Recursively sort partitions
-        sort(A, left, l - 1);
-        sort(A, l + 1, g - 1);
-        sort(A, g + 1, right);
-    }
-}
-
-void swap(int[] A, int i, int j) {
-    final int tmp = A[i]; A[i] = A[j]; A[j] = tmp;
-}
-		 */
+		// Esse aqui eu n soube implementar XD da uma colher de chá pra nóis! KKKKKKKK
 	return array;
 	}
 }
