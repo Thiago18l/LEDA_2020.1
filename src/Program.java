57dec729124d848c6f8627f5ba232d8725aeee55
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-09-29 23:54:29 -0300

    refactor: change main file to use WriteFile method

--- src/Program.java
+++ src/Program.java
@@ -11,15 +11,12 @@ public class Program {
         Store store = new Store();
         String path = "/home/thiago/Documentos/Dev/PROJETO_LEDA_v1/arquivo.txt";
         String output = "/home/thiago/Documentos/Dev/PROJETO_LEDA_v1/ordenado.txt";
-        System.out.println("OI");
         store.ReadFile(path);
         System.out.println(store.size());
 
 
         dados = Arrays.copyOf(store.getData(), store.size());
         algorithms.quicksortMedianaDeTresAlfabetica(dados);
-        for (int i = 0; i < dados.length; i++) {
-            System.out.println(dados[i].getLength() + " " + dados[i].getOcorrencia() + " " + dados[i].getPassword());
-        }
+        store.WriteFile(output, dados);
     }
 }
